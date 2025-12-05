01e57f9da6336f9c143db2cf595f7d7d248ac6d7
donfebriano <donfebriano1282@gmail.com>
2025-12-05 07:55:20 +0700

    Implement GET endpoint for holidays data

---
+++ api/holidays/route.js
@@ -0,0 +1,15 @@
+import holidays from "../../../data/holidays.json" assert { type: "json" };
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const year = searchParams.get("year");
+
+  let result = holidays;
+
+  if (year) {
+    result = holidays.filter((h) => h.date.startsWith(year));
+  }
+
+  return Response.json(result);
+}
+
